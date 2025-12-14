11a8f880b0ad5f8391902b7d5693c68cf9da053b
TatsuyaM2667 <TatsuyaM2667@users.noreply.github.com>
2025-12-14 23:03:07 +0900

    contact zone , skills zone

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,35 @@
+import { IconContext } from 'react-icons'
+import { FaGithub, FaTwitter, FaDiscord, FaInstagram} from 'react-icons/fa';
+
 interface ContactProps {
     id?: string;
 }
+
 const Contact = (props: ContactProps) => {
   return (
-    <div id={props.id}>
-      contact zone
+    <div id={props.id} className="contact-zone">
+      <h2>Contact</h2>
+      <div className="sns-icons">
+      <IconContext.Provider value={{ color: '#ccc', size: '100px' }}>
+
+        <a href="https://github.com/TatsuyaM2667" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={70} />
+        </a>
+        <a href="https://x.com/TatsuyaM2667" target="_blank" rel="noopener noreferrer">
+          <FaTwitter size={70} />
+        </a>
+        <a href="https://discord.gg/example" target="_blank" rel="noopener noreferrer">
+          <FaDiscord size={70} />
+        </a>
+        <a href="https://www.instagram.com/tatsuya.m._.07/?__pwa=1" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={70} />
+        </a>
+        </IconContext.Provider>
+
+      </div>
+      
     </div>
+
   )
 }
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,8 +1,34 @@
 
 const Skills = () => {
   return (
-    <div>
-      skills zone
+    <div className="skills-zone">
+
+      <h2>Skills</h2>
+      <h3>Programing Languages</h3>
+      <img alt="my skills" src="https://skillicons.dev/icons?theme=dark&perline=9&i=c,cpp,js,ts,python" />
+
+      <h3>Frontend Development</h3>
+      <img alt="my skills" src="https://skillicons.dev/icons?theme=dark&perline=9&i=html,css,tailwind,react" />
+
+      <h3>Mobile App Development</h3>
+      <img alt="My Skills" src="https://go-skill-icons.vercel.app/api/icons?i=reactnative,android&titles=true&theme=dark" />
+
+      <h3>Framework</h3>
+      <img alt="my skills" src="https://skillicons.dev/icons?theme=dark&perline=15&i=electron,next" />
+      <img alt="My Skills" src="https://go-skill-icons.vercel.app/api/icons?i=expo,googleappsscript&titles=true&theme=dark" />
+      <br />
+
+      <h3>Tools</h3>
+      <img alt="my skills" src="https://skillicons.dev/icons?theme=dark&perline=15&i=windows,arduino,vscode,git,github,vite" />
+      <img alt="My Skills" src="https://go-skill-icons.vercel.app/api/icons?i=googleappsscript,androidstudio&titles=true&theme=dark" />
+
+      <h3>Database</h3>
+      <img alt="My Skills" src="https://go-skill-icons.vercel.app/api/icons?i=firebase,cloudflare&titles=true&theme=dark" />
+
+      <h3>Devops</h3>
+      <img alt="my skills" src="https://skillicons.dev/icons?theme=dark&perline=7&i=gcp,aws" />
+
+
     </div>
   )
 }
