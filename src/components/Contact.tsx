5eed305ef2532225ed50337b214c5f091b5bd623
TatsuyaM2667 <TatsuyaM2667@users.noreply.github.com>
2025-12-16 23:55:41 +0900

    h3

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,15 +14,19 @@ const Contact = (props: ContactProps) => {
 
         <a href="https://github.com/TatsuyaM2667" target="_blank" rel="noopener noreferrer">
           <FaGithub size={70} />
+          <h3>GitHub</h3>
         </a>
         <a href="https://x.com/TatsuyaM2667" target="_blank" rel="noopener noreferrer">
           <FaTwitter size={70} />
+          <h3>Twitter</h3>
         </a>
         <a href="https://discord.gg/example" target="_blank" rel="noopener noreferrer">
           <FaDiscord size={70} />
+          <h3>Discord</h3>
         </a>
         <a href="https://www.instagram.com/tatsuya.m._.07/?__pwa=1" target="_blank" rel="noopener noreferrer">
           <FaInstagram size={70} />
+          <h3>Instagram</h3>
         </a>
         </IconContext.Provider>
 
