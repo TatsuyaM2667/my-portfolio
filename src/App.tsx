58b891c775814191dc9ab17236a289751720470a
TatsuyaM2667 <TatsuyaM2667@users.noreply.github.com>
2025-12-16 23:14:24 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Skills from './components/Skills';
 import Works from './components/Works';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
+import Profile from './components/Profile';
 function App() {
 
   return (
     <>
     <Hero/>
     <Skills/>
+    <Profile/>
     <Works id="works"/>
     <Experience/>
 <Contact id="contact"/>
